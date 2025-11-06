91369b1006b2d71599d56b542545481a40326f3c
Federico Capaldo <federico.capaldo@hotmail.com>
2025-11-06 21:07:52 +0000

    commented out all javascript and added ul and li elements straight into html with the addition of a data attribute

--- javascript/application.js
+++ javascript/application.js
@@ -1,21 +1,21 @@
-const abbaNames = ["Bjorn", "Benny", "Anni-frid", "Agnetha"];
+// const abbaNames = ["Bjorn", "Benny", "Anni-frid", "Agnetha"];
 
-const abbaNamesUnorderedList = document.createElement("ul");
+// const abbaNamesUnorderedList = document.createElement("ul");
 
-abbaNames.forEach((abbaMember) => {
-  const abbaMemberListItem = document.createElement("li");
-  abbaMemberListItem.innerHTML = abbaMember;
-  abbaNamesUnorderedList.appendChild(abbaMemberListItem);
-})
+// abbaNames.forEach((abbaMember) => {
+//   const abbaMemberListItem = document.createElement("li");
+//   abbaMemberListItem.innerHTML = abbaMember;
+//   abbaNamesUnorderedList.appendChild(abbaMemberListItem);
+// })
 
-const containerElement = document.querySelector(".container");
-containerElement.appendChild(abbaNamesUnorderedList);
+// const containerElement = document.querySelector(".container");
+// containerElement.appendChild(abbaNamesUnorderedList);
 
 
-const abbaMembersListElements = document.querySelectorAll("li");
-abbaMembersListElements.forEach((abbaMember) => {
-  abbaMember.innerHTML += " 🎵"
-})
+// const abbaMembersListElements = document.querySelectorAll("li");
+// abbaMembersListElements.forEach((abbaMember) => {
+//   abbaMember.innerHTML += " 🎵"
+// })
 
-const abbaMembersUnorderedList = document.querySelector('ul');
-abbaMembersUnorderedList.style.listStyleType = "upper-roman";
+// const abbaMembersUnorderedList = document.querySelector('ul');
+// abbaMembersUnorderedList.style.listStyleType = "upper-roman";
