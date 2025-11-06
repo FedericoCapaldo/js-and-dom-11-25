5f7f88a45e69bc9420a83c448a38c9c4f63e15b3
Federico Capaldo <federico.capaldo@hotmail.com>
2025-11-06 20:33:59 +0000

    example of of javascript to add a list of elements to the DOM

--- javascript/application.js
+++ javascript/application.js
@@ -1,1 +1,11 @@
-console.log("Hello from JavaScript!");
+const abba = ["Bjorn", "Benny", "Anni-frid", "Agnetha"];
+
+const list = document.createElement("ul");
+
+abba.forEach((member) => {
+  const listItem = document.createElement("li");
+  listItem.innerHTML = member;
+  list.appendChild(listItem);
+})
+
+document.body.insertAdjacentElement('beforeend', list);
